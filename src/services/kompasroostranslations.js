26c5c4a341af60a70cc9ea6ac0f69fb21495910c
Robbert Wethmar <robbert@digitalica.nl>
2019-04-27 21:32:40 +0200

    prepare for translation to de and fr

--- src/services/kompasroostranslations.js
+++ src/services/kompasroostranslations.js
@@ -1,16 +1,30 @@
 let nl = {};
 let en = {};
+let de = {};
+let fr = {};
 
 nl.WEIGHT_INTRO = 'Stel uw exitgewicht in, en zie welke parachutes geschikt zijn volgens het BVR.';
 en.WEIGHT_INTRO = 'Set your exit weight and see what parachutes are allowed according to Dutch regulations.';
+de.WEIGHT_INTRO = "";
+fr.WEIGHT_INTRO = "";
+
 
 nl.EXPERIENCE_INTRO = 'Stel uw skydive ervaring en zie welke parachutes geschikt zijn volgens het BVR.';
 en.EXPERIENCE_INTRO = 'Set your skydiving experience and see what parachutes are acceptable according to Dutch regulations.';
+de.EXPERIENCE_INTRO = "";
+fr.EXPERIENCE_INTRO = "";
+
 
 nl.EXPERIENCE_WELCOME = 'Welkom bij de Skydive Kompasroos!';
 en.EXPERIENCE_WELCOME = 'Welcome at the Skydive Kompasroos!';
+de.EXPERIENCE_WELCOME = "";
+fr.EXPERIENCE_WELCOME = "";
+
 nl.EXPERIENCE_FREE = "Een gratis app voor skydivers.";
 en.EXPERIENCE_FREE = "A free app for skydivers.";
+de.EXPERIENCE_FREE = "";
+fr.EXPERIENCE_FREE = "";
+
 
 nl.COUNTRIES = {
   "de": "Duitsland",
@@ -34,10 +48,38 @@ en.COUNTRIES = {
   "ua": "Ukraine",
   "us": "United States"
 };
+de.COUNTRIES = {
+  "de": "",
+  "es": "",
+  "fr": "",
+  "cz": "",
+  "nl": "",
+  "nz": "",
+  "sa": "",
+  "ua": "",
+  "us": ""
+};
+fr.COUNTRIES = {
+  "de": "",
+  "es": "",
+  "fr": "",
+  "cz": "",
+  "nl": "",
+  "nz": "",
+  "sa": "",
+  "ua": "",
+  "us": ""
+};
 nl.EXITWEIGHT = 'Exitgewicht';
 en.EXITWEIGHT = 'Exit weight';
+de.EXITWEIGHT = "";
+fr.EXITWEIGHT = "";
+
 nl.EXPERIENCE = 'Ervaring';
 en.EXPERIENCE = 'Experience';
+de.EXPERIENCE = "";
+fr.EXPERIENCE = "";
+
 nl.EXPERIENCE_NEEDED = [
   "Unknown",
   "Geen",
@@ -58,134 +100,304 @@ en.EXPERIENCE_NEEDED = [
   "At least 1000 jumps",
   "At least 1200 jumps, of which 200 with a cat VI cross-braced canopy"
 ];
+de.EXPERIENCE_NEEDED = [
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  ""
+];
+fr.EXPERIENCE_NEEDED = [
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  ""
+];
 nl.FILTER = 'Filter';
 en.FILTER = 'Filter';
+de.FILTER = "";
+fr.FILTER = "";
+
 nl.FILTER_ALL = 'Alles';
 en.FILTER_ALL = 'All';
+de.FILTER_ALL = "";
+fr.FILTER_ALL = "";
+
 nl.FILTER_COMMON = 'Gangbaar';
 en.FILTER_COMMON = 'Common';
+de.FILTER_COMMON = "";
+fr.FILTER_COMMON = "";
+
 nl.FILTER_AROUND = 'Rond categorie';
 en.FILTER_AROUND = 'Around category';
+de.FILTER_AROUND = "";
+fr.FILTER_AROUND = "";
+
 nl.SEARCH = 'Zoek';
 en.SEARCH = 'Search';
+de.SEARCH = "";
+fr.SEARCH = "";
+
 nl.SORTING = 'Sortering';
 en.SORTING = 'Sorting';
+de.SORTING = "";
+fr.SORTING = "";
+
 nl.SORTING_NAME = 'Naam';
 en.SORTING_NAME = 'Name';
+de.SORTING_NAME = "";
+fr.SORTING_NAME = "";
+
 nl.SORTING_MANUFACTURER = 'Fabrikant';
 en.SORTING_MANUFACTURER = 'Manufacturer';
+de.SORTING_MANUFACTURER = "";
+fr.SORTING_MANUFACTURER = "";
+
 nl.SORTING_CATEGORY = 'Categorie';
 en.SORTING_CATEGORY = 'Category';
+de.SORTING_CATEGORY = "";
+fr.SORTING_CATEGORY = "";
+
 nl.LANGUAGE = "Taal";
 en.LANGUAGE = "Language";
+de.LANGUAGE = "";
+fr.LANGUAGE = "";
+
 nl.LANGUAGE_DUTCH = "Nederlands";
 en.LANGUAGE_DUTCH = "Dutch";
+de.LANGUAGE_DUTCH = "";
+fr.LANGUAGE_DUTCH = "";
+
 nl.LANGUAGE_ENGLISH = "Engels";
 en.LANGUAGE_ENGLISH = "English";
+de.LANGUAGE_ENGLISH = "";
+fr.LANGUAGE_ENGLISH = "";
+
 
 
 // canopy list
 nl.CANOPY_NAME = 'Naam'; // voor search
-en.CANOPY_NAME = 'Name'; // voor search
+en.CANOPY_NAME = 'Name';
+de.CANOPY_NAME = "";
+fr.CANOPY_NAME = "";
+ // voor search
 nl.CANOPY_CATEGORY = 'Categorie'; // voor search
-en.CANOPY_CATEGORY = 'Category'; // voor search
+en.CANOPY_CATEGORY = 'Category';
+de.CANOPY_CATEGORY = "";
+fr.CANOPY_CATEGORY = "";
+ // voor search
 nl.CANOPY_MANUFACTURER = 'Fabrikant';
 en.CANOPY_MANUFACTURER = 'Manufacturer';
+de.CANOPY_MANUFACTURER = "";
+fr.CANOPY_MANUFACTURER = "";
+
 nl.CANOPY_EXPERIENCENEEDED_L = 'Ervaring nodig';
 en.CANOPY_EXPERIENCENEEDED_L = 'Experience needed';
+de.CANOPY_EXPERIENCENEEDED_L = "";
+fr.CANOPY_EXPERIENCENEEDED_L = "";
+
 nl.CANOPY_EXPERIENCENEEDED_S = 'Nodig';
 en.CANOPY_EXPERIENCENEEDED_S = 'Needed';
+de.CANOPY_EXPERIENCENEEDED_S = "";
+fr.CANOPY_EXPERIENCENEEDED_S = "";
+
 nl.CANOPY_CELLS = 'Cellen';
 en.CANOPY_CELLS = 'Cells';
+de.CANOPY_CELLS = "";
+fr.CANOPY_CELLS = "";
+
 nl.CANOPY_FIRSTYEAR_L = 'Eerste jaar productie';
 en.CANOPY_FIRSTYEAR_L = 'First year of production';
+de.CANOPY_FIRSTYEAR_L = "";
+fr.CANOPY_FIRSTYEAR_L = "";
+
 nl.CANOPY_FIRSTYEAR_S = 'Eerste jaar';
 en.CANOPY_FIRSTYEAR_S = 'First year';
+de.CANOPY_FIRSTYEAR_S = "";
+fr.CANOPY_FIRSTYEAR_S = "";
+
 nl.CANOPY_LASTYEAR_L = 'Laatste jaar productie';
 en.CANOPY_LASTYEAR_L = 'Last year of production';
+de.CANOPY_LASTYEAR_L = "";
+fr.CANOPY_LASTYEAR_L = "";
+
 nl.CANOPY_LASTYEAR_S = 'Laatste jaar';
 en.CANOPY_LASTYEAR_S = 'Last year';
+de.CANOPY_LASTYEAR_S = "";
+fr.CANOPY_LASTYEAR_S = "";
+
 nl.CANOPY_SIZE = 'Maat';
 en.CANOPY_SIZE = 'Size';
+de.CANOPY_SIZE = "";
+fr.CANOPY_SIZE = "";
+
 nl.CANOPY_MAXSIZE_L = 'Grootste maat';
 en.CANOPY_MAXSIZE_L = 'Maximal size';
+de.CANOPY_MAXSIZE_L = "";
+fr.CANOPY_MAXSIZE_L = "";
+
 nl.CANOPY_MAXSIZE_S = 'Grootste';
 en.CANOPY_MAXSIZE_S = 'Maximal';
+de.CANOPY_MAXSIZE_S = "";
+fr.CANOPY_MAXSIZE_S = "";
+
 nl.CANOPY_MINSIZE_L = 'Kleinste maat';
 en.CANOPY_MINSIZE_L = 'Minimal size';
+de.CANOPY_MINSIZE_L = "";
+fr.CANOPY_MINSIZE_L = "";
+
 nl.CANOPY_MINSIZE_S = 'Kleinste';
 en.CANOPY_MINSIZE_S = 'Minimal';
+de.CANOPY_MINSIZE_S = "";
+fr.CANOPY_MINSIZE_S = "";
+
 nl.CANOPY_REMARKS = 'Opmerkingen';
 en.CANOPY_REMARKS = 'Remarks';
+de.CANOPY_REMARKS = "";
+fr.CANOPY_REMARKS = "";
+
 nl.CANOPY_BEWARE = 'Let op';
 en.CANOPY_BEWARE = 'Beware';
+de.CANOPY_BEWARE = "";
+fr.CANOPY_BEWARE = "";
+
 nl.CANOPY_WINGLOADWARNING = 'Dit levert je een te hoge wingload op, daarom mag je deze koepel ondanks de categorie toch niet springen.';
 en.CANOPY_WINGLOADWARNING = 'This results in the wingload being too high, so in spite of the category you\'re not allowed to jump this parachute.';
+de.CANOPY_WINGLOADWARNING = "";
+fr.CANOPY_WINGLOADWARNING = "";
+
 nl.CANOPY_CATTOOHIGHWARNING = 'Je hebt nog onvoldoende ervaring om met deze parachute te vliegen. Hij is ingedeeld in een hogere categorie dan jij nu.';
 en.CANOPY_CATTOOHIGHWARNING = 'You lack the experience to fly this canopy. Its categorized higher then you at this point in your jumping career.';
+de.CANOPY_CATTOOHIGHWARNING = "";
+fr.CANOPY_CATTOOHIGHWARNING = "";
+
 nl.CANOPY_SIZEWARNING = 'Dit type parachute kan je vliegen, mits de wingload niet te hoog wordt. Let op je minimum maat.';
 en.CANOPY_SIZEWARNING = 'You can fly this type of canopy, as long as the wingload doesn\'t get too high. Take your minimum size into account';
+de.CANOPY_SIZEWARNING = "";
+fr.CANOPY_SIZEWARNING = "";
+
 nl.CANOPY_NOTCATEGORIZEDYET = 'Nog niet ingedeeld. Tot die tijd geldt categorie: ';
 en.CANOPY_NOTCATEGORIZEDYET = 'Not categorized yet. Until then the category is: ';
+de.CANOPY_NOTCATEGORIZEDYET = "";
+fr.CANOPY_NOTCATEGORIZEDYET = "";
+
 
 
 nl.MANUFACTURER_NAME = "Naam";
 en.MANUFACTURER_NAME = "Name";
+de.MANUFACTURER_NAME = "";
+fr.MANUFACTURER_NAME = "";
+
 nl.MANUFACTURER_COUNTRY = "Land";
 en.MANUFACTURER_COUNTRY = "Country";
+de.MANUFACTURER_COUNTRY = "";
+fr.MANUFACTURER_COUNTRY = "";
+
 nl.MANUFACTURER_REMARKS = 'Opmerkingen';
 en.MANUFACTURER_REMARKS = 'Remarks';
+de.MANUFACTURER_REMARKS = "";
+fr.MANUFACTURER_REMARKS = "";
+
 
 
 nl.MENU_ABOUT = 'Over';
 en.MENU_ABOUT = 'About';
+de.MENU_ABOUT = "";
+fr.MENU_ABOUT = "";
+
 nl.MENU_SETTINGS = 'Instellingen';
 en.MENU_SETTINGS = 'Settings';
+de.MENU_SETTINGS = "";
+fr.MENU_SETTINGS = "";
+
 nl.MENU_HELP = 'Help';
 en.MENU_HELP = 'Help';
+de.MENU_HELP = "";
+fr.MENU_HELP = "";
+
 
 nl.SEARCH_FILTERWARNING = "Parachutes verborgen door filter!";
 en.SEARCH_FILTERWARNING = "Parachutes hidden by filter!";
+de.SEARCH_FILTERWARNING = "";
+fr.SEARCH_FILTERWARNING = "";
+
 
 nl.RESULT_CAT = 'Je valt in categorie';
 en.RESULT_CAT = 'Your category is';
+de.RESULT_CAT = "";
+fr.RESULT_CAT = "";
+
 nl.RESULT_ALLOWED_L = 'Toegestane parachutes';
 en.RESULT_ALLOWED_L = 'Allowed canopies';
+de.RESULT_ALLOWED_L = "";
+fr.RESULT_ALLOWED_L = "";
+
 nl.RESULT_ALLOWED_S = 'Toegestaan';
 en.RESULT_ALLOWED_S = 'Allowed';
+de.RESULT_ALLOWED_S = "";
+fr.RESULT_ALLOWED_S = "";
+
 nl.RESULT_MAXCAT = 'Maximaal categorie';
 en.RESULT_MAXCAT = 'At most category';
+de.RESULT_MAXCAT = "";
+fr.RESULT_MAXCAT = "";
+
 nl.RESULT_MINAREA_L = 'Minimale maat';
 en.RESULT_MINAREA_L = 'Minimum size';
+de.RESULT_MINAREA_L = "";
+fr.RESULT_MINAREA_L = "";
+
 nl.RESULT_MINAREA_S = 'Minimaal';
 en.RESULT_MINAREA_S = 'Minimum';
+de.RESULT_MINAREA_S = "";
+fr.RESULT_MINAREA_S = "";
+
 nl.RESULT_EFFMINAREA_L = 'Min. bij';
 en.RESULT_EFFMINAREA_L = 'Min. at';
+de.RESULT_EFFMINAREA_L = "";
+fr.RESULT_EFFMINAREA_L = "";
+
 nl.RESULT_EFFMINAREA_S = 'Bij ';
 en.RESULT_EFFMINAREA_S = 'At';
+de.RESULT_EFFMINAREA_S = "";
+fr.RESULT_EFFMINAREA_S = "";
+
 nl.RESULT_MAXWINGLOAD_L = 'Maximale wingload';
 en.RESULT_MAXWINGLOAD_L = 'Maximum wingload';
+de.RESULT_MAXWINGLOAD_L = "";
+fr.RESULT_MAXWINGLOAD_L = "";
+
 nl.RESULT_MAXWINGLOAD_S = 'Maximaal';
 en.RESULT_MAXWINGLOAD_S = 'Maximum';
+de.RESULT_MAXWINGLOAD_S = "";
+fr.RESULT_MAXWINGLOAD_S = "";
+
 
 
 nl.CANOPIESBYTITLE = "Koepels van";
 en.CANOPIESBYTITLE = "Canopies by";
+de.CANOPIESBYTITLE = "";
+fr.CANOPIESBYTITLE = "";
+
 
 nl.NOLIMIT = 'Geen limiet';
 en.NOLIMIT = 'No limit';
+de.NOLIMIT = "";
+fr.NOLIMIT = "";
+
 
 
 nl.EXPERIENCE_DONE = 'Klaar, mijn ervaring is ingesteld!';
 en.EXPERIENCE_DONE = 'Done, my experience is set!';
-
-
-// nl.RESULT_BEGINNER = 'Beginner';
-// en.RESULT_BEGINNER = 'Beginner';
-// nl.RESULT_INTERMEDIATE = 'Ervaren';
-// en.RESULT_INTERMEDIATE = 'Experienced';
-// nl.RESULT_SKYGOD = 'Sky god';
-// en.RESULT_SKYGOD = 'Sky god';
+de.EXPERIENCE_DONE = "";
+fr.EXPERIENCE_DONE = "";
 
 
 nl.JUMPERCATEGORIES = [
@@ -208,35 +420,91 @@ en.JUMPERCATEGORIES = [
   "1000 jumps or more, at least 100 jumps in the last 12 months",
   "1200 jumps or more; at least 200 with a cat VI cross-braced canopy including at least 100 in the last 12 months"
 ];
+de.JUMPERCATEGORIES = [
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  ""
+];
+fr.JUMPERCATEGORIES = [
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  ""
+];
 
 
 nl.MEASURE_JUMPS = "sprongen";
 en.MEASURE_JUMPS = "jumps";
+de.MEASURE_JUMPS = "";
+fr.MEASURE_JUMPS = "";
+
 nl.MEASURE_KG = "kg";
 en.MEASURE_KG = "kg";
+de.MEASURE_KG = "";
+fr.MEASURE_KG = "";
+
 
 nl.SLIDER_WEIGHT_L = 'Exitgewicht';
 en.SLIDER_WEIGHT_L = 'Exit weight';
+de.SLIDER_WEIGHT_L = "";
+fr.SLIDER_WEIGHT_L = "";
+
 nl.SLIDER_WEIGHT_S = 'Gewicht';
 en.SLIDER_WEIGHT_S = 'Weight';
+de.SLIDER_WEIGHT_S = "";
+fr.SLIDER_WEIGHT_S = "";
+
 nl.SLIDER_JUMPSTOTAL_L = 'Aantal sprongen totaal';
 en.SLIDER_JUMPSTOTAL_L = 'Total number of jumps';
+de.SLIDER_JUMPSTOTAL_L = "";
+fr.SLIDER_JUMPSTOTAL_L = "";
+
 nl.SLIDER_JUMPSTOTAL_S = 'Totaal';
 en.SLIDER_JUMPSTOTAL_S = 'Total';
+de.SLIDER_JUMPSTOTAL_S = "";
+fr.SLIDER_JUMPSTOTAL_S = "";
+
 nl.SLIDER_JUMPSXBRACED_L = 'Totaal aantal cross braced sprongen';
 en.SLIDER_JUMPSXBRACED_L = 'Total number of jumps on cross based canopies';
+de.SLIDER_JUMPSXBRACED_L = "";
+fr.SLIDER_JUMPSXBRACED_L = "";
+
 nl.SLIDER_JUMPSXBRACED_S = 'X Braced';
 en.SLIDER_JUMPSXBRACED_S = 'X Braced';
+de.SLIDER_JUMPSXBRACED_S = "";
+fr.SLIDER_JUMPSXBRACED_S = "";
+
 nl.SLIDER_JUMPSLAST12MONTHS_L = 'Aantal sprongen in de laatste 12 maanden';
 en.SLIDER_JUMPSLAST12MONTHS_L = 'Number of jumps in the last 12 months';
+de.SLIDER_JUMPSLAST12MONTHS_L = "";
+fr.SLIDER_JUMPSLAST12MONTHS_L = "";
+
 nl.SLIDER_JUMPSLAST12MONTHS_S = 'Laatste 12 maanden';
 en.SLIDER_JUMPSLAST12MONTHS_S = 'Last 12 months';
+de.SLIDER_JUMPSLAST12MONTHS_S = "";
+fr.SLIDER_JUMPSLAST12MONTHS_S = "";
+
 
 nl.TABLE_WINGLOADTITLE = "Wingload in lbs/sqft";
 en.TABLE_WINGLOADTITLE = "Wingload in lbs/sqft";
+de.TABLE_WINGLOADTITLE = "";
+fr.TABLE_WINGLOADTITLE = "";
+
 
 nl.SUMMARY_TITLE = "Toegestane parachutes";
 en.SUMMARY_TITLE = "Allowed parachutes";
+de.SUMMARY_TITLE = "";
+fr.SUMMARY_TITLE = "";
+
 
 
 module.exports = {
