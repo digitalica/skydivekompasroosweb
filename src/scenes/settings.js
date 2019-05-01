6f4a35de1336d6d47e51121ee064e4e53a838351
Robbert Wethmar <robbert@digitalica.nl>
2019-05-01 13:11:51 +0200

    add flags

--- src/scenes/settings.js
+++ src/scenes/settings.js
@@ -6,6 +6,8 @@ import RadioGroup from '@material-ui/core/RadioGroup';
 import React from "react";
 import {withStyles} from '@material-ui/core/styles';
 
+import Flag from 'react-world-flags';
+
 import C from "../services/kompasroosconstants";
 import T from "../services/kompasroostranslations";
 
@@ -23,6 +25,17 @@ const styles = theme => ({
     paddingTop: theme.spacing.unit * 2,
     paddingLeft: theme.spacing.unit * 2,
     paddingRight: theme.spacing.unit * 2,
+  },
+  languageselect: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  flagcolumn: {
+    height: "100%",
+    display: "flex",
+    justifyContent: "space-around",
+    flexDirection: "column",
   },
 });
 
@@ -64,18 +77,30 @@ class Settings extends React.Component {
             </RadioGroup>
 
             <FormLabel component="legend">{T[language].LANGUAGE}</FormLabel>
-            <RadioGroup
-              aria-label={T[language].LANGUAGE}
-              name={T[language].LANGUAGE}
-              className={classes.group}
-              value={language}
-              onChange={this.props.updateLanguage}
-            >
-              <FormControlLabel value={C.LANGUAGE_NL} control={<Radio/>} label={T[language].LANGUAGE_DUTCH}/>
-              <FormControlLabel value={C.LANGUAGE_EN} control={<Radio/>} label={T[language].LANGUAGE_ENGLISH}/>
-              <FormControlLabel value={C.LANGUAGE_DE} control={<Radio/>} label={T[language].LANGUAGE_GERMAN}/>
-              <FormControlLabel value={C.LANGUAGE_FR} control={<Radio/>} label={T[language].LANGUAGE_FRENCH}/>
-            </RadioGroup>
+            <div className={classes.languageselect}>
+              <RadioGroup
+                aria-label={T[language].LANGUAGE}
+                name={T[language].LANGUAGE}
+                className={classes.group}
+                value={language}
+                onChange={this.props.updateLanguage}
+              >
+                <FormControlLabel value={C.LANGUAGE_NL} control={<Radio/>}
+                                  label={T[language].LANGUAGE_DUTCH + " / " + T["nl"].LANGUAGE_DUTCH}/>
+                <FormControlLabel value={C.LANGUAGE_EN} control={<Radio/>}
+                                  label={T[language].LANGUAGE_ENGLISH + " / " + T["en"].LANGUAGE_ENGLISH}/>
+                <FormControlLabel value={C.LANGUAGE_DE} control={<Radio/>}
+                                  label={T[language].LANGUAGE_GERMAN + " / " + T["de"].LANGUAGE_GERMAN}/>
+                <FormControlLabel value={C.LANGUAGE_FR} control={<Radio/>}
+                                  label={T[language].LANGUAGE_FRENCH + " / " + T["fr"].LANGUAGE_FRENCH}/>
+              </RadioGroup>
+              <div className={classes.flagcolumn}>
+                <Flag code="nl" height="16" width="24"/>
+                <Flag code="gb" height="16" width="24"/>
+                <Flag code="de" height="16" width="24"/>
+                <Flag code="fr" height="16" width="24"/>
+              </div>
+            </div>
           </FormControl>
         </div>
       </div>
