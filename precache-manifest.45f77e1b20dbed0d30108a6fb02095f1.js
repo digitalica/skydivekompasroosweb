279faa3863f3bcd8990eba0da4f8c1f533d1037a
Robbert Wethmar <robbert@digitalica.nl>
2020-08-09 17:28:13 +0200

    Updates

---
+++ precache-manifest.45f77e1b20dbed0d30108a6fb02095f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50ef0d8de0139df5d174b597498270ca",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4bd30e13753834496366",
+    "url": "./static/css/main.75452755.chunk.css"
+  },
+  {
+    "revision": "015db6506eaf26c2d599",
+    "url": "./static/js/2.a634c96c.chunk.js"
+  },
+  {
+    "revision": "4e3f8d4bad1db285aba0fceffdc7b967",
+    "url": "./static/js/2.a634c96c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4bd30e13753834496366",
+    "url": "./static/js/main.0463ff46.chunk.js"
+  },
+  {
+    "revision": "31a0429fe47f21695d61",
+    "url": "./static/js/runtime-main.2d36a32b.js"
+  }
+]);
