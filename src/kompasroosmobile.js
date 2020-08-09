459e83977cf2fd6d6c10048cca8fbe868674a443
digitalica <digitalica@users.noreply.github.com>
2020-08-09 14:01:38 +0000

    deploy: c81447a27f87bd27aea21e643cc47fccf4e1b7a6

---
+++ src/kompasroosmobile.js
@@ -0,0 +1,92 @@
+import React from "react";
+import {BrowserRouter, Redirect, Switch} from "react-router-dom";
+
+import Header from "./components/header";
+import Main from "./scenes/main";
+import About from "./scenes/about";
+import Setfilter from "./scenes/setfilter";
+import Setsorting from "./scenes/setsorting";
+import Setlanguage from "./scenes/setlanguage";
+import SetJumps from "./scenes/setjumps";
+import SetWeight from "./scenes/setweight";
+import Canopy from "./scenes/canopy";
+import Manufacturer from "./scenes/manufacturer";
+import Footer from "./components/footer";
+
+import KompasroosRoute from "./kompasroosroute";
+
+function KompasroosMobile(props) {
+  return (
+    <BrowserRouter>
+      <div>
+        <Header
+          {...props}
+        />
+        <Switch>
+          <KompasroosRoute
+            exact path="/"
+            component={Main}
+
+            {...props}
+          />
+          <KompasroosRoute
+            path="/about"
+            component={About}
+
+            {...props}
+          />
+          <KompasroosRoute
+            path="/setfilter"
+            component={Setfilter}
+
+            {...props}
+          />
+          <KompasroosRoute
+            path="/setsorting"
+            component={Setsorting}
+
+            {...props}
+          />
+          <KompasroosRoute
+            path="/setlanguage"
+            component={Setlanguage}
+
+            {...props}
+          />
+          <KompasroosRoute
+            path="/setjumps"
+            component={SetJumps}
+
+            {...props}
+          />
+          <KompasroosRoute
+            path="/setweight"
+            component={SetWeight}
+
+            {...props}
+          />
+          <KompasroosRoute
+            path="/canopy/:slug"
+            component={Canopy}
+
+            {...props}
+          />
+          <KompasroosRoute
+            path="/manufacturer/:slug"
+            component={Manufacturer}
+
+            {...props}
+          />
+          <Redirect to="/"/>
+        </Switch>
+        <Footer
+          {...props}
+        />
+      </div>
+    </BrowserRouter>
+
+  );
+
+}
+
+export default KompasroosMobile
