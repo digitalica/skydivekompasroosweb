b966dc42fc33142a796f8ee90643d8ec2c88554e
digitalica <digitalica@users.noreply.github.com>
2020-08-09 15:44:19 +0000

    deploy: 68a18f12c4b1bc148a8fa9816f550c305b0d98d1

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './App.css';
+
+
+import KompasroosApp from './kompasroosapp'
+
+
+function App() {
+  return (
+    <div className="App">
+      <KompasroosApp/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/scenes/setweight.js
@@ -0,0 +1,65 @@
+import React from "react";
+import {withStyles} from '@material-ui/core/styles';
+
+import KompasroosSlider from "../components/kompasroosslider";
+import Wingloadtable from "../components/wingloadtable";
+import Summary from "../components/summary";
+
+import C from "../services/kompasroosconstants";
+import T from "../services/kompasroostranslations";
+
+const styles = theme => ({
+  root: {
+    textAlign: "left",
+    paddingTop: 64,
+    paddingBottom: 150,
+  },
+  text: {
+    maxWidth: C.MAXWIDTH,
+    marginLeft: "auto",
+    marginRight: "auto",
+    paddingTop: theme.spacing.unit * 2,
+    paddingLeft: theme.spacing.unit * 2,
+    paddingRight: theme.spacing.unit * 2,
+  },
+});
+
+
+class SetWeight extends React.Component {
+  render() {
+    const {classes, language} = this.props;
+    return (
+      <div className={classes.root}>
+        <div className={classes.text}>
+          {T[language].WEIGHT_INTRO}
+        </div>
+        <KompasroosSlider
+          labels={T[language].SLIDER_WEIGHT_S}
+          labell={T[language].SLIDER_WEIGHT_L}
+          value={this.props.exitWeight}
+          min={C.EXITWEIGHT_MIN}
+          max={C.EXITWEIGHT_MAX}
+          onChange={this.props.updateExitWeight}
+          measure={T[language].MEASURE_KG}
+        />
+        <div className={classes.text}>
+          <Wingloadtable
+            language={language}
+            exitWeight={this.props.exitWeight}
+            category={this.props.category}
+          />
+          <br/>
+          <br/>
+          <Summary
+            language={language}
+            category={this.props.category}
+            exitWeight={this.props.exitWeight}
+          />
+        </div>
+      </div>
+    )
+  }
+}
+
+export default withStyles(styles)(SetWeight);
+
