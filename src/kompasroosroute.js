0609b9468409418e0957fa23763f7890b8dfdd25
digitalica <digitalica@users.noreply.github.com>
2020-08-09 14:42:18 +0000

    deploy: 276bfc61e56409cbd68451d4137df475ffbe8ff4

---
+++ src/kompasroosroute.js
@@ -0,0 +1,18 @@
+import {Route} from "react-router-dom";
+import GetExperience from "./scenes/getexperience";
+import React from "react";
+
+function KompasroosRoute({component: Component, ...rest}) {
+  return (
+    <Route {...rest} render={(props) => {
+      // console.log(rest.showWelcome ? "show welcome" : "ready");
+      // console.log(JSON.stringify(rest));
+      // console.log(JSON.stringify(props));
+      return rest.showWelcome
+        ? <GetExperience {...rest} />
+        : <Component slug={props.match.params.slug} {...rest}/>
+    }}/>
+  );
+}
+
+export default KompasroosRoute
