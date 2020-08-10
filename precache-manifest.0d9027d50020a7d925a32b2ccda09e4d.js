c50fea73d0915fd5f72859dfbf926266e330286f
digitalica <digitalica@users.noreply.github.com>
2020-08-10 22:37:31 +0000

    deploy: f7fe9c634c51ba3e7ad8ba89e3e9e73f5bf0376d

---
+++ precache-manifest.0d9027d50020a7d925a32b2ccda09e4d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adce04fcf0857c27e11a1daec667e439",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5358a669b1ef8d992053",
+    "url": "./static/css/main.75452755.chunk.css"
+  },
+  {
+    "revision": "015db6506eaf26c2d599",
+    "url": "./static/js/2.a634c96c.chunk.js"
+  },
+  {
+    "revision": "4e3f8d4bad1db285aba0fceffdc7b967",
+    "url": "./static/js/2.a634c96c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5358a669b1ef8d992053",
+    "url": "./static/js/main.976cb195.chunk.js"
+  },
+  {
+    "revision": "31a0429fe47f21695d61",
+    "url": "./static/js/runtime-main.2d36a32b.js"
+  }
+]);
