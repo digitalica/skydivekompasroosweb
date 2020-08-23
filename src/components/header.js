digitalica/skydivekompasroosweb

commit 4aae164c8cd4840fe5cdb8fc37083d89fdb21f18

add language to URL (for seo / forward)

src/components/header.js
@@ -23,6 +23,7 @@ import {withStyles} from '@material-ui/core/styles';
 
 import Autotext from "./autotext";
 
+import C from "../services/kompasroosconstants";
 import T from "../services/kompasroostranslations";
 
 
@@ -121,36 +122,36 @@ class TopAppBar extends React.Component {
     // console.log('go search ' + this.state.showSearch);
     if (!this.props.showWelcome) {
       this.setState({showSearch: true});
-      if (this.props.location.pathname !== "/") {
-        this.props.history.push('/');
-      }
+      // if (this.props.location.pathname !== "/") {
+      //   this.props.history.push('/');
+      // }
     }
   };
 
 
   goFilter = () => {
     this.closeMenu();
-    this.props.history.push('/setfilter')
+    this.props.history.push('/' + this.props.language + '/setfilter')
   };
 
   goSorting = () => {
     this.closeMenu();
-    this.props.history.push('/setsorting')
+    this.props.history.push('/' + this.props.language + '/setsorting')
   };
 
   goLanguage = () => {
     this.closeMenu();
-    this.props.history.push('/setlanguage')
+    this.props.history.push('/' + this.props.language + '/setlanguage')
   };
 
   goAbout = () => {
     this.closeMenu();
-    this.props.history.push('/about')
+    this.props.history.push('/' + this.props.language + '/about')
   };
 
 
   goBack = () => {
-    this.props.history.push('/')
+    this.props.history.push('/' + this.props.language + '/')
   };
 
 
@@ -158,11 +159,10 @@ class TopAppBar extends React.Component {
     const {classes, language} = this.props;
     const {anchorEl} = this.state;
 
-
     let topLeftButton = null;
     // console.log(this.props.location.pathname)
     if (!this.props.showWelcome) {
-      if (this.props.location.pathname === "/") {
+      if (C.isJustLanguagePath(this.props.location.pathname)) {
         // console.log('menu');
         topLeftButton =
           <IconButton className={classes.menuButton} color="inherit" aria-label="Menu" onClick={this.openMenu}>
@@ -179,7 +179,7 @@ class TopAppBar extends React.Component {
 
     let topRightButton = null;
     if (!this.props.showWelcome) {
-      if (this.props.location.pathname === "/" && this.state.showSearch) {
+      if (C.isJustLanguagePath(this.props.location.pathname) && this.state.showSearch) {
         topRightButton =
           <IconButton
             className={classes.searchButton}
@@ -203,7 +203,7 @@ class TopAppBar extends React.Component {
     }
 
     let headerCenter;
-    if (this.props.location.pathname === "/" && this.state.showSearch) {
+    if (C.isJustLanguagePath(this.props.location.pathname) && this.state.showSearch) {
       headerCenter = (
         <div className={classes.search}>
           <InputBase

src/components/footer.js
@@ -74,13 +74,13 @@ class BottomAppBar extends React.Component {
 
   toggleSetWeight = () => {
     if (!this.props.showWelcome) {
-      this.props.history.push(this.togglePage('/setweight'))
+      this.props.history.push(this.togglePage('/' + this.props.language + '/setweight'))
     }
   };
 
   toggleSetJumps = () => {
     if (!this.props.showWelcome) {
-      this.props.history.push(this.togglePage('/setjumps'))
+      this.props.history.push(this.togglePage('/' + this.props.language + '/setjumps'))
     }
   };
 

src/components/list.js
@@ -141,6 +141,7 @@ class List extends React.Component {
 
           canopyList.push(
             <Listelement
+              key={canopy.id}
               language={language}
               canopy={canopy}
               category={this.props.category}
@@ -156,6 +157,8 @@ class List extends React.Component {
           lastGroup = canopy.calculationcategory;
           canopyList.push(
             <Listelement
+              key={canopy.id}
+              language={language}
               canopy={canopy}
               category={this.props.category}
               exitWeight={this.props.exitWeight}
@@ -168,6 +171,7 @@ class List extends React.Component {
           canopyList.push(
             <Listelement
               key={canopy.id}
+              language={language}
               canopy={canopy}
               category={this.props.category}
               exitWeight={this.props.exitWeight}

src/components/listelement.js
@@ -138,7 +138,7 @@ class ListElement extends React.Component {
 
         element = (
           <li className={classes[classAcceptability]}>
-            <Link to={'/canopy/' + canopy.slug}>
+            <Link to={'/' + language + '/canopy/' + canopy.slug}>
               <table>
                 <tbody>
                 <tr>
@@ -165,7 +165,7 @@ class ListElement extends React.Component {
       case C.SORTING_CATEGORY:
         element = (
           <li className={classes[classAcceptability]}>
-            <Link to={'/canopy/' + canopy.slug}>
+            <Link to={'/' + language + '/canopy/' + canopy.slug}>
               <table>
                 <tbody>
                 <tr>
@@ -193,7 +193,7 @@ class ListElement extends React.Component {
       default:
         element = (
           <li className={classes[classAcceptability]}>
-            <Link to={'/canopy/' + canopy.slug}>
+            <Link to={'/' + language + '/canopy/' + canopy.slug}>
               <table>
                 <tbody>
                 <tr>

src/kompasroosapp.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
 
+import {BrowserRouter, Route, Redirect, Switch} from "react-router-dom";
+
 import {withCookies} from 'react-cookie';
 
 import {MuiThemeProvider, createMuiTheme} from '@material-ui/core/styles';
@@ -66,12 +68,16 @@ class KompasroosApp extends Component {
       default:
         break;
     }
-    // console.log('langs ' + browserLanguage +  "  " + defaultLanguage + " " + navigator.languages + " |  " + navigator.language +  " |  "+ navigator.userLanguage);
+
+    let languageCookie = cookies.get(C.COOKIE_LANGUAGE)
+    if (!C.isValidLanguage(languageCookie)) {
+      languageCookie = ''
+    }
 
     this.state = {
       filter: cookies.get(C.COOKIE_FILTER) || C.FILTER_ALL,
       sorting: cookies.get(C.COOKIE_SORTING) || C.SORTING_NAME,
-      language: cookies.get(C.COOKIE_LANGUAGE) || defaultLanguage ,
+      defaultLanguage: languageCookie || defaultLanguage ,
       showWelcome: !showList,
       searchText: '',
       exitWeight: exitWeght,
@@ -82,12 +88,16 @@ class KompasroosApp extends Component {
     };
   }
 
-
+  
   updateLanguage = (event) => {
     const {cookies} = this.props;
-    let language = event.target.value;
-    cookies.set(C.COOKIE_LANGUAGE, language, C.cookieOptions());
-    this.setState({language: language});
+    let newLanguage = event.target.value;
+    cookies.set(C.COOKIE_LANGUAGE, newLanguage, C.cookieOptions());
+    this.setState({language: newLanguage});
+    // actually switch language, we're not in a router, so no history available :-(
+    const oldLangRegEx = /\/..\/setlanguage/
+    const newLangPath = '/' + newLanguage + '/setlanguage'
+    window.location.href = window.location.href.replace(oldLangRegEx, newLangPath)
   };
 
   updateSorting = (event) => {
@@ -141,66 +151,78 @@ class KompasroosApp extends Component {
   render() {
     return (
       <MuiThemeProvider theme={theme}>
-        <Media query="(max-width: 1280px)">
-          {matches =>
-            matches ? (
-              <KompasroosMobile
-                theme={theme}
+        <BrowserRouter>
+          <Switch>
+            <Route path="/:language(de|en|fr|nl)"
 
-                isMobile={true}
+render={({ match, props, location }) => (
+  <Media query="(max-width: 1280px)">
+    
+    {matches =>
+      matches ? (
+        <KompasroosMobile
+          theme={theme}
 
-                showWelcome={this.state.showWelcome}
-                welcomeDone={this.welcomeDone}
+          isMobile={true}
 
-                language={this.state.language}
-                filter={this.state.filter}
-                sorting={this.state.sorting}
+          showWelcome={this.state.showWelcome}
+          welcomeDone={this.welcomeDone}
 
-                updateLanguage={this.updateLanguage}
-                updateFilter={this.updateFilter}
-                updateSorting={this.updateSorting}
+          language={match.params.language}
+          filter={this.state.filter}
+          sorting={this.state.sorting}
 
-                searchText={this.state.searchText}
-                onSearchChange={this.updateSearchText}
+          updateLanguage={this.updateLanguage}
+          updateFilter={this.updateFilter}
+          updateSorting={this.updateSorting}
 
-                totalJumps={this.state.totalJumps}
-                jumpsLast12Months={this.state.jumpsLast12Months}
-                xbracedJumps={this.state.xbracedJumps}
-                updateJumps={this.updateJumps}
-                category={this.state.category}
-                exitWeight={this.state.exitWeight}
-                updateExitWeight={this.updateExitWeight}
-              />
-            ) : (
-              <KompasroosDesktop
-                theme={theme}
+          searchText={this.state.searchText}
+          onSearchChange={this.updateSearchText}
 
-                isMobile={false}
+          totalJumps={this.state.totalJumps}
+          jumpsLast12Months={this.state.jumpsLast12Months}
+          xbracedJumps={this.state.xbracedJumps}
+          updateJumps={this.updateJumps}
+          category={this.state.category}
+          exitWeight={this.state.exitWeight}
+          updateExitWeight={this.updateExitWeight}
+        />
+      ) : (
+        <KompasroosDesktop
+          theme={theme}
 
-                showWelcome={this.state.showWelcome}
-                welcomeDone={this.welcomeDone}
+          isMobile={false}
 
-                language={this.state.language}
-                filter={this.state.filter}
-                sorting={this.state.sorting}
+          showWelcome={this.state.showWelcome}
+          welcomeDone={this.welcomeDone}
 
-                updateLanguage={this.updateLanguage}
-                updateFilter={this.updateFilter}
-                updateSorting={this.updateSorting}
+          language={match.params.language}
+          filter={this.state.filter}
+          sorting={this.state.sorting}
 
-                searchText={this.state.searchText}
-                onSearchChange={this.updateSearchText}
+          updateLanguage={this.updateLanguage}
+          updateFilter={this.updateFilter}
+          updateSorting={this.updateSorting}
 
-                totalJumps={this.state.totalJumps}
-                jumpsLast12Months={this.state.jumpsLast12Months}
-                xbracedJumps={this.state.xbracedJumps}
-                updateJumps={this.updateJumps}
-                category={this.state.category}
-                exitWeight={this.state.exitWeight}
-                updateExitWeight={this.updateExitWeight}
-              />
-            )}
-        </Media>
+          searchText={this.state.searchText}
+          onSearchChange={this.updateSearchText}
+
+          totalJumps={this.state.totalJumps}
+          jumpsLast12Months={this.state.jumpsLast12Months}
+          xbracedJumps={this.state.xbracedJumps}
+          updateJumps={this.updateJumps}
+          category={this.state.category}
+          exitWeight={this.state.exitWeight}
+          updateExitWeight={this.updateExitWeight}
+        />
+      )}
+  </Media>
+)}
+
+            />
+            <Redirect to={"/" + this.state.defaultLanguage + "/"} />
+          </Switch>
+        </BrowserRouter>
       </MuiThemeProvider>
     );
   }

src/kompasroosdesktop.js
@@ -26,6 +26,7 @@ const styles = theme => ({
 
 
 function KompasroosDesktop(props) {
+  const {language} = props
 
   return (
     <BrowserRouter>
@@ -35,48 +36,48 @@ function KompasroosDesktop(props) {
         />
         <Switch>
           <KompasroosRoute
-            exact path="/"
+            exact path={"/" + language + "/"}
             component={Main}
 
             {...props}
           />
           <KompasroosRoute
-            path="/about"
+            path={"/" + language + "/about" }
             component={About}
 
             {...props}
           />
           <KompasroosRoute
-            path="/setfilter"
+            path={"/" + language + "/setfilter"}
             component={Setfilter}
 
             {...props}
           />
           <KompasroosRoute
-            path="/setsorting"
+            path={"/" + language + "/setsorting"}
             component={Setsorting}
 
             {...props}
           />
           <KompasroosRoute
-            path="/setlanguage"
+            path={"/" + language + "/setlanguage"}
             component={Setlanguage}
 
             {...props}
           />
           <KompasroosRoute
-            path="/canopy/:slug"
+            path={"/" + language + "/canopy/:slug"}
             component={Canopy}
 
             {...props}
           />
           <KompasroosRoute
-            path="/manufacturer/:slug"
+            path={"/" + language + "/manufacturer/:slug"}
             component={Manufacturer}
 
             {...props}
           />
-          <Redirect to="/"/>
+          <Redirect to={"/" + language + "/"} />
         </Switch>
       </div>
     </BrowserRouter>

src/kompasroosmobile.js
@@ -16,6 +16,7 @@ import Footer from "./components/footer";
 import KompasroosRoute from "./kompasroosroute";
 
 function KompasroosMobile(props) {
+  const {language} = props
   return (
     <BrowserRouter>
       <div>
@@ -24,60 +25,60 @@ function KompasroosMobile(props) {
         />
         <Switch>
           <KompasroosRoute
-            exact path="/"
+            exact path={"/" + language +"/"}
             component={Main}
 
             {...props}
           />
           <KompasroosRoute
-            path="/about"
+            path={"/" + language + "/about"}
             component={About}
 
             {...props}
           />
           <KompasroosRoute
-            path="/setfilter"
+            path={"/" + language + "/setfilter"}
             component={Setfilter}
 
             {...props}
           />
           <KompasroosRoute
-            path="/setsorting"
+            path={"/" + language + "/setsorting"}
             component={Setsorting}
 
             {...props}
           />
           <KompasroosRoute
-            path="/setlanguage"
+            path={"/" + language + "/setlanguage"}
             component={Setlanguage}
 
             {...props}
           />
           <KompasroosRoute
-            path="/setjumps"
+            path={"/" + language + "/setjumps"}
             component={SetJumps}
 
             {...props}
           />
           <KompasroosRoute
-            path="/setweight"
+            path={"/" + language + "/setweight"}
             component={SetWeight}
 
             {...props}
           />
           <KompasroosRoute
-            path="/canopy/:slug"
+            path={"/" + language + "/canopy/:slug"}
             component={Canopy}
 
             {...props}
           />
           <KompasroosRoute
-            path="/manufacturer/:slug"
+            path={"/" + language + "/manufacturer/:slug"}
             component={Manufacturer}
 
             {...props}
           />
-          <Redirect to="/"/>
+          <Redirect to={"/" + language + "/"} />
         </Switch>
         <Footer
           {...props}

src/scenes/canopy.js
@@ -153,6 +153,9 @@ function Canopy(props) {
   const {classes, language, slug, category, exitWeight} = props;
   // console.log(JSON.stringify(props));
 
+  console.log('canopy props ' + JSON.stringify(Object.keys(props)))
+
+
   const canopy = kompasroosData.canopies[kompasroosData.slugs[slug]];
 
   let canopyCellsRow = null;
@@ -374,7 +377,7 @@ function Canopy(props) {
             <tbody>
             {warningRow}
             <tr className={classes.linkrow}
-                onClick={() => props.history.push("/manufacturer/" + canopy.manufacturerslug)}>
+                onClick={() => props.history.push("/" + language + "/manufacturer/" + canopy.manufacturerslug)}>
               <td className={classes.canopycategory}>{T[language].CANOPY_MANUFACTURER}:</td>
               <td className={classes.canopydetails}>
                 {canopy.manufacturername}

commit 8f8b2cd14574e301844e59db02646bfc2a59c23e

fix search functionality
broken in commit 4aae164

src/components/header.js
@@ -122,9 +122,11 @@ class TopAppBar extends React.Component {
     // console.log('go search ' + this.state.showSearch);
     if (!this.props.showWelcome) {
       this.setState({showSearch: true});
-      // if (this.props.location.pathname !== "/") {
-      //   this.props.history.push('/');
-      // }
+      // if we are not in the main screen (but for example canopy screen)
+      // return to main screen (showing the list) for search
+      if (!C.isJustLanguagePath(this.props.location.pathname)) {
+        this.props.history.push('/' + this.props.language + '/');
+      }
     }
   };
 